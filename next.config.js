cc2ff84a4043bd2c7ab3f53b5d6a1f913b9f9e1e
Joshua Bellamy <github@smysnk.com>
2026-01-16 11:33:18 -0500

    Externalize sequelize and sqlite3 for Next build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  serverExternalPackages: ["sequelize", "sqlite3"]
 };
 
 module.exports = nextConfig;
